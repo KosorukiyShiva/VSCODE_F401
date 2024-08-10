d4c13714c01c9d4c4f799b5cf2005828ebdc4a4b
KosorukiyShiva <alcatelandry8@gmail.com>
2024-08-10 22:51:34 +0300

    changed settings.json and finished ClockInit, PC13LED, PC13LEDHi and PC13LEDLow, not tested

--- Src/main.c
+++ Src/main.c
@@ -22,9 +22,71 @@
 #include "stm32f401xc.h"
 void ClockInit(void){
     RCC->CR |= (1 << RCC_CR_HSEON_Pos);
+    __IO int StartUpCounter;
+    for (StartUpCounter = 0;     ; StartUpCounter++)
+    {
+        if(RCC->CR & (1 << RCC_CR_HSERDY_Pos)){
+            break;
+        }
+        if(StartUpCounter > 0x1000){
+            RCC->CR &= ~(1 << RCC_CR_HSEON_Pos);
+        }
+    }
+    
+    RCC->CFGR |= (0x02 << RCC_CFGR_MCO2_Pos);
+    RCC->CFGR |= (0x00 << RCC_CFGR_MCO2PRE_Pos);
+    RCC->PLLCFGR |= (0x04 << RCC_PLLCFGR_PLLQ_Pos);
+    RCC->PLLCFGR |= (0x63 << RCC_PLLCFGR_PLLN_Pos)
+    RCC->PLLCFGR |= (0x00 << RCC_PLLCFGR_PLLP_Pos); 
+    RCC->CR |= (1 << RCC_CR_PLLON_Pos);
+    
+    for (StartUpCounter = 0;    ; StartUpCounter++)
+    {
+        if(RCC->CR & (1 << RCC_CR_PLLON_Pos)){
+            break;
+        }
+        if(StartUpCounter > 0x1000){
+            RCC->CR &= ~(1 << RCC_CR_PLLON_Pos);
+            RCC->CR &= ~(1 << RCC_CR_HSEON_Pos); 
+        }
+    }
+    
+    FLASH->ACR |= (0x02 << FLASH_ACR_LATENCY_Pos);
+    
+    RCC->CFGR |= (0x00 << RCC_CFGR_PPRE1_Pos);
+    RCC->CFGR |= (0x00 << RCC_CFGR_PPRE2_Pos);
+    RCC->CFGR |= (0x00 << RCC_CFGR_HPRE_Pos);
+    
+    while ((RCC->CFGR & RCC_CFGR_SWS_Msk) != (0x02 << RCC_CFGR_SWS_Pos)){}
+    RCC->CR &= ~(1 << RCC_CR_HSION_Pos);
+    
+    return 0;    
 }
+
+void PC13LED(void){
+    RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOCEN_Pos);
+	GPIOC->MODER &= ~(GPIO_MODER_MODER13);
+	GPIOC->MODER |= (0x02 << GPIO_MODER_MODER13);
+	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT13);
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPD13);
+}
+
+void PC13LEDHi(void){
+    GPIOC->BSRR |= (1<<13);
+}
+
+void PC13LEDLow(void){
+    GPIOC->BSRR |= ~(1<<13);
+}
+
 int main(void)
 {
-    /* Loop forever */
-	for(;;);
+    ClockInit();
+    PC13LED();
+	for(    ;   ;   ){
+        PC13LEDHi();
+        for (int i = 0; i < 0x4000; i++){}
+        PC13LEDLow();
+        for (int i = 0; i < 0x4000; i++){}
+    }
 }
