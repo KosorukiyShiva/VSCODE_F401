da9a6cbcd1db5b669d5f441a038317631e8bd554
KosorukiyShiva <alcatelandry8@gmail.com>
2024-08-19 01:01:55 +0300

    the locknit and 13 function was completed for the board with stm32f401ccu, the 13  does not blink(

--- Src/main.c
+++ Src/main.c
@@ -60,25 +60,25 @@
 //    
 //    return 0;    
 //}
-//
-//void PC13LED(void){
-//    RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOCEN_Pos);
-//	GPIOC->MODER &= ~(GPIO_MODER_MODER13);
-//	GPIOC->MODER |= (0x02 << GPIO_MODER_MODER13_Pos);
-//	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT13);`
-//	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPD13);
-//}
-//
-//void PC13LEDHi(void){
-//    GPIOC->BSRR |= (1<<13);
-//}
-//
-//void PC13LEDLow(void){
-//    GPIOC->BSRR |= ~(1<<13);
-//}
+
+void PC13LED(void){
+    RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOCEN_Pos);
+	GPIOC->MODER &= ~(GPIO_MODER_MODER13);
+	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT13);
+	GPIOC->MODER |= (0x02 << GPIO_MODER_MODER13_Pos);
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPD13);
+}
+
+void PC13LEDHi(void){
+    GPIOC->BSRR |= (1<<13);
+}
+
+void PC13LEDLow(void){
+    GPIOC->BSRR |= ~(1<<13);
+}
 
 void ClockInit(void){
-    //RCC->CR = 0x0000XX81;
+    //RCC->CR = 0x00006981;
     RCC->CR |= (1 << RCC_CR_HSION_Pos);
     RCC->CR |= (1 << RCC_CR_HSEON_Pos);
     __IO int StrtUpcntr;
@@ -103,7 +103,7 @@ void ClockInit(void){
         }
     }
     RCC->PLLCFGR = 0x24003010;              //see EASYPLL
-    RCC->PLLCFGR |= (1 << RCC_PLLCFGR_PLLSRC_Pos); 
+    RCC->PLLCFGR |= (0x01 << RCC_PLLCFGR_PLLSRC_Pos); 
     RCC->PLLCFGR |= (0x192 << RCC_PLLCFGR_PLLN_Pos);
     RCC->PLLCFGR |= (0x08 << RCC_PLLCFGR_PLLM_Pos);
     RCC->PLLCFGR |= (0x03 << RCC_PLLCFGR_PLLP_Pos);
@@ -111,9 +111,7 @@ void ClockInit(void){
     FLASH->ACR |= (0x02 << FLASH_ACR_LATENCY_Pos);
     RCC->CR |= (1 << RCC_CR_PLLON_Pos);
     for(StrtUpcntr = 0; ;   StrtUpcntr++){
-        if (1 & (1 << RCC_CR_PLLRDY_Pos)){
-            break;
-        }
+        if ((RCC->CR & RCC_CR_PLLRDY_Msk) & (0x01 << RCC_CR_PLLRDY_Pos))break;
         if(StrtUpcntr > 0x1000){
             RCC->CR &= ~(1 << RCC_CR_PLLON_Pos);
             RCC->CR &= ~(1 << RCC_CR_HSION_Pos);
@@ -124,17 +122,21 @@ void ClockInit(void){
     RCC->CFGR |= (0x00 << RCC_CFGR_HPRE_Pos);
     RCC->CFGR |= (0x00 << RCC_CFGR_PPRE2_Pos);
     RCC->CFGR |= (0x02 << RCC_CFGR_SW_Pos);
-    for(StrtUpcntr = 0; ; StrtUpcntr++){
-        if(0x02 & (0x02 << RCC_CFGR_SWS_Pos)){
-            break;
-        }
-        if (StrtUpcntr > 0x1000){
-            RCC->CFGR &= ~(0x02 << RCC_CFGR_SW_Pos);
-            RCC->CR &= ~(1 << RCC_CR_PLLON_Pos);
-            RCC->CR &= ~(1 << RCC_CR_HSION_Pos);
-            return 1;
-        }
-    }
+    while ((RCC->CFGR & RCC_CFGR_SWS_Msk) != (0x02 << RCC_CFGR_SWS_Pos)){}
+    RCC->CR &= ~(1 << RCC_CR_HSION_Pos);
+    return 0;    
+    //for(StrtUpcntr = 0; ; StrtUpcntr++){
+    //    if(0x02 & (0x02 << RCC_CFGR_SWS_Pos)){
+    //        break;
+    //    }
+    //    if (StrtUpcntr > 0x1000){
+    //        RCC->CFGR &= ~(0x02 << RCC_CFGR_SW_Pos);
+    //        RCC->CR &= ~(1 << RCC_CR_PLLON_Pos);
+    //        RCC->CR &= ~(1 << RCC_CR_HSION_Pos);
+    //        return 1;
+    //    }
+    //}
+
 
 }
 
